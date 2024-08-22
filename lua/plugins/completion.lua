5a28ca08fe8104df6e9a45c4677ef909c5a65bba
Leandro Neves <leandrnvs@outlook.com>
2024-08-22 18:47:07 -0300

    colorscheme added, tab and s-tab to run through completion

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,12 +1,12 @@
 return {
-  'hrsh7th/nvim-cmp',
+  "hrsh7th/nvim-cmp",
   dependencies = {
-    'hrsh7th/cmp-nvim-lsp',
-    'hrsh7th/cmp-vsnip',
-    'hrsh7th/vim-vsnip',
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-vsnip",
+    "hrsh7th/vim-vsnip",
   },
   config = function()
-    local cmp = require'cmp'
+    local cmp = require("cmp")
 
     cmp.setup({
       snippet = {
@@ -19,18 +19,32 @@ return {
         documentation = cmp.config.window.bordered(),
       },
       mapping = cmp.mapping.preset.insert({
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        ["<Tab>"] = function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          else
+            fallback()
+          end
+        end,
+        ["<S-Tab>"] = function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          else
+            fallback()
+          end
+        end,
       }),
       sources = cmp.config.sources({
-        { name = 'nvim_lsp' },
-        { name = 'vsnip' },
+        { name = "nvim_lsp" },
+        { name = "vsnip" },
       }, {
-        { name = 'buffer' },
-      })
+        { name = "buffer" },
+      }),
     })
-  end
+  end,
 }
